c76d26980ecd2083c60e084b49d80e2cb9c95c73
Stefan Papp <stefan.papp@data-wizard.net>
2021-07-12 10:23:15 +0400

    chores

--- install_brew.sh
+++ install_brew.sh
@@ -1,35 +1,35 @@
 #!/bin/sh
-brew install zsh tig thefuck wget git hub hh pyenv jenv scalaenv zplug git-extras tmux
-brew cask install visual-studio-code
-brew cask install 1password 
-brew cask install bartender         
-brew cask install beyond-compare   
-brew cask install duefocus          
-brew cask install fantastical      
-brew cask install font-fira-code  
-brew cask install fork           
-brew cask install iina          
-brew cask install iterm2       
-brew cask install java        
-brew cask install joplin     
-brew cask install keka      
-brew cask install kindle   
-brew cask install omnigraffle
-brew cask install oracle-jdk 
-brew cask install powershell 
-brew cask install pycharm-ce 
-brew cask install qbittorrent
-brew cask install skype      
-brew cask install slack     
-brew cask install spotify          
-brew cask install sublime-text  
-brew cask install telegram       
-brew cask install tunnelblick    
-brew cask install virtualbox    
-brew cask install vlc     
-brew cask install webex  
-brew cask install whatsapp 
-brew cask install zoom
+brew install bat zsh tig thefuck wget git hub hh pyenv jenv scalaenv zplug git-extras tmux
+brew install --cask visual-studio-code
+brew install --cask 1password
+brew  install --cask bartender
+brew  install --cask beyond-compare
+brew  install --cask duefocus
+brew  install --cask fantastical
+brew  install --cask font-fira-code
+brew  install --cask fork
+brew  install --cask iina
+brew  install --cask iterm2
+brew  install --cask java
+brew  install --cask joplin
+brew  install --cask keka
+brew  install --cask kindle
+brew  install --cask omnigraffle
+brew  install --cask oracle-jdk
+brew  install --cask powershell
+brew  install --cask pycharm-ce
+brew  install --cask qbittorrent
+brew  install --cask skype
+brew  install --cask slack
+brew  install --cask spotify
+brew  install --cask sublime-text
+brew  install --cask telegram
+brew  install --cask tunnelblick
+brew  install --cask virtualbox
+brew  install --cask vlc
+brew  install --cask webex
+brew  install --cask whatsapp
+brew  install --cask zoom
 
 # formulars
 brew install awscli bat curl k9s diff-so-fancy docker docker-machine exa fd gh git git-extras gnu-getopt go helm hstr htop iproute2mac jenv jq kotlin lnav maven midnight-commander minikube most navi neovim nmap pyenv-virtualenv ranger ripgrep sbt scala scalaenv speedtest-cli terraform thefuck tig tmux vifm vim eksctl wget zplug zsh zsh-syntax-highlighting
